af37aceb0649e823e32691c013f601f4ff8cd958
sonhyejoo <seongwoo.cho@gmail.com>
2023-04-10 16:41:50 -0400

    ===

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -10,7 +10,7 @@ const rootReducer = combineReducers({});
 
 let enhancer;
 
-if (process.env.NODE_ENV == "production") {
+if (process.env.NODE_ENV === "production") {
   enhancer = applyMiddleware(thunk);
 } else {
   const logger = require("redux-logger").default;
