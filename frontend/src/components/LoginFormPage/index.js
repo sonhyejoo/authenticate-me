7651ac708676f83c59d3ee1f19a0c16062809254
sonhyejoo <seongwoo.cho@gmail.com>
2023-04-10 23:24:06 -0400

    Add functional LoginFormPage component

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -1,38 +1,54 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { login } from "../../store/session";
+import { Redirect } from "react-router-dom";
 
 const LoginFormPage = () => {
   const dispatch = useDispatch();
   const [cred, setCred] = useState("");
   const [pass, setPass] = useState("");
+  const [errors, setErrors] = useState([]);
+  const sessionUser = useSelector((state) => state.session.user);
+
+  if (sessionUser) {
+    return <Redirect to="/" />;
+  }
 
   const updateCred = (e) => setCred(e.target.value);
   const updatePass = (e) => setPass(e.target.value);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const user = {
-      credential: cred,
-      password: pass,
-    };
-    return dispatch(login(user));
+    return dispatch(login({ credential: cred, password: pass })).catch(
+      async (res) => {
+        const data = await res.json();
+        if (data && data.errors) setErrors(data.errors);
+      }
+    );
   };
 
   return (
     <>
       <h1>Login</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
+        <ul>
+          {errors.map((error, idx) => (
+            <li key={idx}>{error}</li>
+          ))}
+        </ul>
         <div>
-          <label htmlFor="credential">Credential: </label>
-          <input type="text" value={cred} onChange={updateCred} />
-          {/* Credential {"(Username or Email)"} */}
+          <label htmlFor="credential">
+            Credential:
+            <input type="text" value={cred} onChange={updateCred} />
+          </label>
         </div>
         <div>
-          <label htmlFor="password">Password: </label>
-
-          <input type="password" value={pass} onChange={updatePass} />
+          <label htmlFor="password">
+            Password:
+            <input type="password" value={pass} onChange={updatePass} />
+          </label>
         </div>
+        <button>Log In</button>
       </form>
     </>
   );
